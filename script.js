cd089b1802ed8812634ebdb2132148441fc112f2
shimamuzavvir <shima1997ibrahim@gmail.com>
2024-01-29 20:24:28 +0400

    first

---
+++ script.js
@@ -0,0 +1,32 @@
+var txt = document.getElementById("text");
+txt.innerText=10;
+setTimeout(function(){
+    txt.innerText=9;
+    setTimeout(function(){
+        txt.innerText=8;
+        setTimeout(function(){
+            txt.innerText=7;
+            setTimeout(function(){
+                txt.innerText=6;
+                setTimeout(function(){
+                    txt.innerText=5;
+                    setTimeout(function(){
+                        txt.innerText=4;
+                        setTimeout(function(){
+                            txt.innerText=3;
+                            setTimeout(function(){
+                                txt.innerText=2;
+                                setTimeout(function(){
+                                    txt.innerText=1;
+                                    setTimeout(function(){
+                                        txt.innerHTML="<h1>🥳🎈Happy Independence Day🎈🎉</h1>"},1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+
+    },1000)
+},1000)
